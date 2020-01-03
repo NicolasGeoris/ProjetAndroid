18e73618a29baa62872dab3129fc2b767d53192f
Nicolas Georis <ngeoris@gmail.com>
2020-01-03 17:59:53 +0100

    Recycler View fonctionnel : servira a afficher la liste des rôles du jeu

--- app/src/main/java/com/example/projetandroid/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/projetandroid/ui/notifications/NotificationsFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.projetandroid.R
+import java.util.*
 
 class NotificationsFragment : Fragment() {
 
@@ -19,13 +23,21 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_liste_roles)
+        recyclerView?.layoutManager = LinearLayoutManager(view?.context)
+
+        val roles = listOf("Villageois", "Loup", "Petite Fille", "Chasseur", "Voyante")
+
+        val adapter = MyAdapter(roles)
+        recyclerView?.adapter = adapter
+
+
     }
 }
