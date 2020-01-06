1b93f94d276eeda3b23d6ddef32f872df4c2fa4e
sauvardg <56915005+sauvardg@users.noreply.github.com>
2020-01-06 18:25:28 +0100

    change FloatingActionButton color and shape

--- app/src/main/java/com/example/projetandroid/ui/jouer/JouerFragment.kt
+++ app/src/main/java/com/example/projetandroid/ui/jouer/JouerFragment.kt
@@ -1,16 +1,13 @@
 package com.example.projetandroid.ui.jouer
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.example.projetandroid.R
 import kotlinx.android.synthetic.main.fragment_jouer.*
-import kotlinx.android.synthetic.main.fragment_jouer.view.*
 
 class JouerFragment : Fragment() {
 
